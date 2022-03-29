43167eff6ed2100ce32e56771aaa3b5a73d6ca4d
KRISH <66765984+krish8001@users.noreply.github.com>
2022-03-29 15:52:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,12 +37,11 @@ mongoose.connect(URI, {
 })
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+
+  
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
-}
 
 
 
